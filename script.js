0b9d2e0c653039cec3e2f1c7bbd2bd562b30a0c2
Soumik <soumik20020906+github@gmail.com>
2024-10-22 19:18:41 +0530

    bug fixed

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const themeswitch=document.getElementById('themeswitch');
 let darktheme = document.cookie.includes('dark');
 if (darktheme) {
     document.body.classList.add('dark');
+    themeswitch.checked=false;
 } else {
     document.body.classList.remove('dark');
+    themeswitch.checked=true;
+    document.cookie = `theme=light;max-age=${60*60*24*365*2};SameSite=Lax;`;
 }
 themeswitch.addEventListener('change', () => {
     darktheme = !darktheme;
     document.body.classList.toggle('dark');
-    document.cookie = darktheme ? 'dark' : 'light';
+    document.cookie = `theme=${darktheme ? 'dark' : 'light'};max-age=${60*60*24*365*2};SameSite=Lax;`;
 });
